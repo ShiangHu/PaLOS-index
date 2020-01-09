578cbbc73f69f74b0274ebe1e63464a476649619
shianghu <shiandyhu@gmail.com>
2020-01-09 14:21:58 +0800

    add nd=3

--- sim/dipole_pick.m
+++ sim/dipole_pick.m
@@ -1,18 +1,26 @@
-function d = dipole_pick(Gridloc,nd)
-% pick the dipoles for simulation from the iEEG cortex
+function d = dipole_pick(coordinates,nd)
+% DIPOLE_PICK pick the dipoles for simulation from the iEEG cortex
+% here the sEEG activities are taken as the dipolar time series  
 % Input
 %         nd: # of dipoles
+% coordinates: coordinates of the sEEG channels
+
 switch nd
     case 1
-        d = 7019;
+        d = 152;  % emperically selected from bst plot
         
     case 2
-        d1 = 21822; % right frontal
-        d2 = 1821;     % left occiptal
-        d = [d1 d2];
+%         d = [1182 830]; % right frontal - left occiptal 
+        d = [1058 1059]; % right occipital
+        
+    case 3
+        
+        d = [1058 1059 1060];
+        
         
     otherwise    
-        d = randsample(size(Gridloc,1),nd);
+        rng('default');
+        d = randsample(size(coordinates,1),nd);
 
 end
 
