facbf3b27d9f78067d31d66788dc4eee4a1d9da0
shianghu <shiandyhu@gmail.com>
2020-01-09 14:27:36 +0800

    ouput more qc indexes

--- sim/palos_sim.m
+++ sim/palos_sim.m
@@ -1,29 +1,43 @@
 
-%
-% eeglabstart;
-% brainstormstart('');
+% eeglabstart; BSTstart('');
+addpath(genpath(cd),genpath('/media/shu/hdd/iEEG study'));
 clean;
-addpath(genpath(cd));
-addpath(genpath('/media/shu/hdd/iEEG study'));
-nd = 1; % 1 - 1772
-
-[K, J, dipinfo] = kjconfig(nd);
-% forward
-V = K*J;
-
-% check the spectopo
-nm = 'Simulation from iEEG';
-psdnm = [nm,'_PSD'];
-elc = 'bst63.xyz';
-eegplot(V,'title',nm,'plottitle',nm,'srate',200,'winlength',20,'eloc_file',elc);
-figure('name',psdnm,'NumberTitle','off'),
-spectopo(V,0,200,'percent',15,'freq',2:2:30,'title',psdnm,'chanlocs',elc,'electrodes','off');
-
-
 fs = 200; % Hz
-fm = 77/2.56; % maximum frequency
+fm = 50; % maximum frequency
 nw = 3;
-pro = qcspectra(V,nw,fs,fm)
+% ndv = unique(round(logspace(0,log10(1772),20))); %nd vector
+ndv = [2 3];
+pro = zeros(length(ndv),1);
+rkr = zeros(length(ndv),4);
+
+for di = 1:length(ndv)
+    
+    nd = ndv(di);
+    [K, J, dipinfo] = kjconfig(nd);
+    % forward
+    V = K*J;
+    
+    % check the spectopo
+    nm = 'Simulation from iEEG';
+    psdnm = [nm,'_PSD'];
+    elc = 'bst63.xyz';
+    eegplot(V,'title',nm,'plottitle',nm,'srate',200,'winlength',20,'eloc_file',elc);
+    figure('name',psdnm,'NumberTitle','off'),
+    [spectra,freqs] = spectopo(V,0,200,'percent',15,'freq',2:2:30,'title',psdnm,'chanlocs',elc,'electrodes','off');
+    
+    
+    
+    % heterogeneity measures
+    
+    [pro(di), rkr(di,:)] = qcspectra(V,nw,fs,fm);
+    
+    [di nd, pro(di), rkr(di,:)]
+     close all;
+     
+%      disp(di); 
+end
+
+figure, semilogx(ndv,pro),xlim([min(ndv) max(ndv)]);
 
 % K = importdata('G_cmi.mat');
 % J = importdata('data/MC0000001_A.mat');
