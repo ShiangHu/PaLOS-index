e8ca6959d257e41eeef8b148d5baab38ea24b2fd
shianghu <shiandyhu@gmail.com>
2020-01-13 20:46:34 +0800

    update

--- sim/loadlslsres.m
+++ sim/loadlslsres.m
@@ -16,3 +16,9 @@ for i=1:length(lslsres)
     
     saveas(gcf,[fdpath,filesep,nm(1:end-4),'.png']);
 end
+
+% view the ic type ratios
+cab; EEG = pop_loadset; viewictypes(EEG);
+
+% view the component from any subject
+cab; EEG = pop_loadset; pop_viewprops(EEG,0,35,{'freqrange', [0.5 50]},[],0)
