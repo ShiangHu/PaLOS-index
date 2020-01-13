c7b878592dd1bffcd810229fa5a6036fe3ec9c4c
shianghu <shiandyhu@gmail.com>
2020-01-13 15:49:50 +0800

    update

--- sim/palos_sim.m
+++ sim/palos_sim.m
@@ -1,12 +1,13 @@
 
 % eeglabstart; BSTstart('');
 addpath(genpath(cd),genpath('/media/shu/hdd/iEEG study'));
-clean;
+clean; eeglab;
+elc = 'bst63.xyz';
 fs = 200; % Hz
 fm = 50; % maximum frequency
 nw = 3;
 % ndv = unique(round(logspace(0,log10(1772),20))); %nd vector
-ndv = [2 3];
+ndv = [100];
 pro = zeros(length(ndv),1);
 rkr = zeros(length(ndv),4);
 
@@ -18,23 +19,24 @@ for di = 1:length(ndv)
     V = K*J;
     
     % check the spectopo
-    nm = 'Simulation from iEEG';
-    psdnm = [nm,'_PSD'];
-    elc = 'bst63.xyz';
+    nm = 'Simulation from iEEG';    psdnm = [nm,'_PSD'];
     eegplot(V,'title',nm,'plottitle',nm,'srate',200,'winlength',20,'eloc_file',elc);
     figure('name',psdnm,'NumberTitle','off'),
     [spectra,freqs] = spectopo(V,0,200,'percent',15,'freq',2:2:30,'title',psdnm,'chanlocs',elc,'electrodes','off');
     
+    % convert .set and runica
+    EEG = pop_loadset(var2set(V,elc,fs)); eeglab redraw
+    EEG = iclabel(EEG);
+    EEG = pop_icflag(EEG);
     
+    % reject components and reconstruct data
     
     % heterogeneity measures
+    [pro(di), rkr(di,:)] = qcspectra(V,nw,fs,fm); 
     
-    [pro(di), rkr(di,:)] = qcspectra(V,nw,fs,fm);
+    disp([di nd, pro(di), rkr(di,:)]);
     
-    [di nd, pro(di), rkr(di,:)]
-     close all;
-     
-%      disp(di); 
+    close all;
 end
 
 figure, semilogx(ndv,pro),xlim([min(ndv) max(ndv)]);
