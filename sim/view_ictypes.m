2cdd7f4c5f5e7f293fbea0f9cfbbc29512d5e268
shianghu <shiandyhu@gmail.com>
2020-01-15 18:51:35 +0800

    add axes and idx

--- sim/view_ictypes.m
+++ sim/view_ictypes.m
@@ -1,11 +1,19 @@
-function view_ictypes(EEG)
+function view_ictypes(EEG,varargin)
 % VIEWICTYPES view the ratios of the IC types after ICLABEL is done
 % stacked barplot horizatally
 
 
-bfh=figure;
-bar(EEG.etc.ic_classification.ICLabel.classifications,'stacked','Horizontal','on');
+if nargin==1
+    bfh=figure;
+    idx=1:size(EEG.icaact,1);
+else
+    bfh=varargin{1}; % figure handle
+    idx=varargin{2};
+end
+
+bar(bfh,EEG.etc.ic_classification.ICLabel.classifications(idx,:),'stacked','Horizontal','on');
 legend({'brain','muscle','eye','heart','line_noise','chan_noise','other'});
-set(gca,'ytick',1:min(size(EEG.icasphere))), set(bfh,'Position',[1986 1 348 973]);
+set(gca,'ytick',1:min(size(EEG.icasphere))), 
+% set(bfh,'Position',[1986 1 348 973]);
 colormap(jet); axis 'tight'; view(180,90); title(EEG.filename);
 end
