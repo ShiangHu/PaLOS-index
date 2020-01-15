6fed051ab3b75aa98760a556ec8ff5b4bb8cc6db
shianghu <shiandyhu@gmail.com>
2020-01-15 18:50:06 +0800

    renamed from loadlslsres

---
+++ sim/view_lslsres.m
@@ -0,0 +1,29 @@
+% examples for view ICs from lsls result
+clean;
+
+lslsres = dir('../lslsres/*.set');
+fdpath = lslsres(1).folder;
+name = {lslsres.name};
+
+
+for i=1:length(lslsres)
+    
+    nm = name{i};
+    
+    EEG = pop_loadset([fdpath,filesep,nm]);
+    
+    close; viewictypes(EEG);
+    
+    saveas(gcf,[fdpath,filesep,nm(1:end-4),'.png']);
+end
+
+% view the ic type ratios
+cab; EEG = pop_loadset; viewictypes(EEG);
+
+% view the component from any subject
+cab;
+sptopt = {'freqrange', [0.5 50]};
+pop_prop_extended(pop_loadset,0,20,nan,sptopt,{},0);
+
+viewsptopt = {'freqrange',[0.39 50]};
+pop_viewprops(EEG,0,1:size(EEG.icaact,1),viewsptopt,[],0);
