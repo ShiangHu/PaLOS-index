2e568a88fb484de47fb5a2ba58241009624eb82a
shianghu <shiandyhu@gmail.com>
2020-01-15 14:41:01 +0800

    rename as view_ictypes.m

--- sim/viewictypes.m
+++
@@ -1,7 +0,0 @@
-function viewictypes(EEG)
-bfh=figure;
-bar(EEG.etc.ic_classification.ICLabel.classifications,'stacked','Horizontal','on');
-legend({'brain','muscle','eye','heart','line_noise','chan_noise','other'});
-set(gca,'ytick',1:min(size(EEG.icasphere))), set(bfh,'Position',[1986 1 348 973]);
-colormap(jet); axis 'tight'; view(180,90); title(EEG.filename);
-end
