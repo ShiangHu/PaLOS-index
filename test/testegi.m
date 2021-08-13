048869a5c7ed105cb8a41f99919deb7aa9e8f917
Shiang Hu <shiandy@gmail.com>
2021-08-13 22:14:46 +0800

    add test file

---
+++ test/testegi.m
@@ -0,0 +1,47 @@
+% This is to check why high pass 1hz filtering increases palosi
+
+% Batch calculating the CPC 
+% initpalos;
+clean;
+rmdir('hbn1ck','s'), mkdir('hbn1ck');
+
+atmgres = dir(fullfile('*hbn1*','**','all*.mat'));
+stepnm = {'EEGOrig','EEGfiltered','EEGICLabel','EEGHighvarred','EEGItpl'};
+
+nstp = length(stepnm);
+nsbj = length(atmgres);
+pro = zeros(nstp,nsbj); % PaLOS index
+
+nw = 3; fs = 500; fmax = 30; fmin=0.99; % paras fpr spt
+
+sbjnm = cell(1,nsbj);
+ext = {'_1O','_2F','_3L','_4H','_5E'};
+
+tic;
+for i= 1:nsbj
+    disp(['>>>>-------------------preprocessing sbj:',blanks(10),num2str(i)]);
+    allpath = atmgres(i).folder;
+    filename = atmgres(i).name;
+    allStep = load(fullfile(allpath,filename));
+    
+    sbj = allpath(end-7:end-3);
+    sbjnm{i} = sbj;
+    goal = fullfile('hbn1ck',sbj);
+    mkdir(goal);
+    
+    for j=1:nstp 
+        data = getfield(allStep,stepnm{j},'data');
+        chanlocs = getfield(allStep,stepnm{j},'chanlocs');
+
+        eleid = data(:,1)~=0&~isnan(data(:,1));
+        data = data(eleid,:);     
+        chanlocs = chanlocs(eleid);
+        
+        svfd = fullfile(goal,[sbj,ext{j}]);
+        pro(j,i) = qcspectra(data,nw,fs,fmax,fmin,chanlocs,svfd);
+        
+    end
+    toc;
+end
+
+save prohbn1 pro sbjnm;
